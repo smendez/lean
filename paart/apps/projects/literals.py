9e9c1c9f198324cfecd4586ea43015ee4550d6fc
Sasha Méndez <sasha.mendez@gmail.com>
2015-06-14 16:54:10 -0400

    Removed project states from literals.

--- paart/apps/projects/literals.py
+++ paart/apps/projects/literals.py
@@ -7,13 +7,3 @@ INFRASTRUCTURE_CHOICES = (
     (INFRASTRUCTURE_NEW, _(u'New')),
     (INFRASTRUCTURE_OLD, _(u'Existing')),
 )
-
-NEW_TASK = 'N'
-IN_PROCESS_TASK = 'P'
-COMPLETED_TASK = 'C'
-
-TASK_STATUS = (
-    (NEW_TASK, _(u'New task')),
-    (IN_PROCESS_TASK, _(u'In process')),
-    (COMPLETED_TASK, _(u'Completed task'))
-)
